7607cdfc48cd1739d6c097444437aa0e6e45756a
Mariella Miranda <mariella@able.co>
2020-03-12 17:35:32 -0500

    removing space before let to appease Ale

--- camel-case/camelCase.js
+++ camel-case/camelCase.js
@@ -4,7 +4,7 @@ function toCamelCase(str){
   
   const divided = str.split(regex);
 
-  for ( let i = 1; i < divided.length; i++) {
+  for (let i = 1; i < divided.length; i++) {
     divided[i] = divided[i].replace(divided[i][0], divided[i][0].toUpperCase());
   }
 
