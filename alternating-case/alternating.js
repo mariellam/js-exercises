54bbe67715904669ae5028dd22466decb77d4f93
Mariella Miranda <mariella@able.co>
2020-03-12 13:01:57 -0500

    Alternate Case Kata

--- alternating-case/alternating.js
+++ alternating-case/alternating.js
@@ -1,3 +1,16 @@
 String.prototype.toAlternatingCase = function () {
-  // Define your method here :)
+  const chars = this.split('');
+  
+  const switchChars = chars.map((element) => {
+    if (element == element.toLowerCase()) {
+      return element.toUpperCase();
+    } else {
+      return element.toLowerCase();
+    }
+  });
+
+  const switchString = switchChars.join('');
+
+  return switchString;
 }
+console.log("hello WORLD 1a2B".toAlternatingCase());
