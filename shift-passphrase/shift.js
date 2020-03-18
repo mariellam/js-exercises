a7c8c34af3f4336b5da19c13e6778d6e0db0c90c
Maku Miranda <mariella.miranda@gmail.com>
2020-03-18 09:02:49 -0500

    Complete steps

--- shift-passphrase/shift.js
+++ shift-passphrase/shift.js
@@ -8,21 +8,34 @@
 
 function playPass(s, n) {
   
-  let firstShift = s.toLowerCase();
-  let secondShift = String;
+  const firstShift = s.toLowerCase();
+  let arrayToShift = [];
 
-  function shiftPass(n) {
-    for (i=0; i<firstShift.length; i++) {
-      if (96 < firstShift.charCodeAt(i) < 123 ) {
-        charToShift = firstShift.charCodeAt(i) + n;
-        console.log(secondShift.fromCharCode(charToShift));
-      } else {
-        charToShift = firstShift.charCodeAt(i);
-        console.log(secondShift.fromCharCode(charToShift));
-      }
+  for (i = 0; i < firstShift.length; i++) {
+    let charToShift = firstShift.charCodeAt(i);
+
+    if (charToShift >= 97 && charToShift < 122) {
+      charToShift = firstShift.charCodeAt(i) + n;
+    } else if (charToShift == 122) {
+      charToShift = 97;
+    } else if (charToShift >= 48 && charToShift < 58) {
+      charToShift = 48 + (57 - firstShift.charCodeAt(i));
+    } else {
+      charToShift;
+    }
+    arrayToShift.push(charToShift);
+  }
+  
+  let shiftedArray = Array.from(arrayToShift, item => String.fromCharCode(item) );
+
+  for (i = 0; i < shiftedArray.length; i++) {
+    if (i % 2 === 0) {
+      shiftedArray[i] = shiftedArray[i].toUpperCase();
     }
   }
 
-  console.log(shiftPass(n));
+  console.log(shiftedArray.reverse().join(' ').toString());
+  //return shiftedArray.reverse().join(' ').toString();
+
 }
-playPass("I LOVE YOU!!!", 1)//"!!!vPz fWpM J");
+playPass("MY GRANMA CAME FROM NY ON THE 23RD OF APRIL 2015", 2);
