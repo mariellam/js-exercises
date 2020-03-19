30e59232c787abfd4975c74c82263c7849e3bad8
Mariella Miranda <mariella@able.co>
2020-03-19 09:26:02 -0500

    WIP Shift Passphrase (#8)
    * WIP Shift Passphrase
    
    * Complete steps
    
    * Tests Passed. Exercise submitted.
    
    * Applying Comments
    
    * Changing Array.From to map

---
+++ shift-passphrase/shift.js
@@ -0,0 +1,34 @@
+function playPass(s, n) {
+
+  const firstShift = s.toLowerCase();
+  const arrayToShift = [];
+
+  for (i = 0; i < firstShift.length; i++) {
+    let charToShift = firstShift.charCodeAt(i);
+
+    if (charToShift >= 97 && charToShift <= 122) {
+      charToShift = firstShift.charCodeAt(i) + n;
+      if (charToShift >= 123) {
+        charToShift = 97 + (charToShift - 123);
+      }
+    } else if (charToShift >= 48 && charToShift < 58) {
+      charToShift = 48 + (57 - firstShift.charCodeAt(i));
+    } else {
+      charToShift;
+    }
+    arrayToShift.push(charToShift);
+  }
+
+  const shiftedArray = arrayToShift.map(item => String.fromCharCode(item));
+
+  for (i = 0; i < shiftedArray.length; i++) {
+    if (i % 2 === 0) {
+      shiftedArray[i] = shiftedArray[i].toUpperCase();
+    }
+  }
+
+  console.log(shiftedArray.reverse().join(''));
+  //return shiftedArray.reverse().join('')
+
+}
+playPass("MY GRANMA CAME FROM NY ON THE 23RD OF APRIL 2015", 2);
