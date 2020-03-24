54765cfa2123627e0244e6c63fa76297272ab200
Mariella Miranda <mariella@able.co>
2020-03-24 15:26:58 -0500

    Convert string to camel case (#6)
    * Convert string to camel case
    
    * removing space before let to appease Ale

---
+++ camel-case/camelCase.js
@@ -0,0 +1,14 @@
+function toCamelCase(str){
+
+  const regex = /[\-]|[\_]/g;
+  
+  const divided = str.split(regex);
+
+  for (let i = 1; i < divided.length; i++) {
+    divided[i] = divided[i].replace(divided[i][0], divided[i][0].toUpperCase());
+  }
+
+  return divided.join('');
+}
+
+toCamelCase("A-B-C");
