92a27d06b7712f792e2a73615cc0c5fd7641b093
Mariella Miranda <mariella@able.co>
2020-03-25 16:00:39 -0500

    Simplify code

--- most-sales/sales.js
+++ most-sales/sales.js
@@ -3,7 +3,16 @@ amounts: [3, 24, 8]
 prices: [199, 299, 399] */
 
 function top3(products, amounts, prices) {
-  const revenue = [];
+
+  const totalRevenues = products.map((product, i) => ({name: product, revenue: amounts[i] * prices[i]}));
+  const sortedRevenues = totalRevenues.sort((a, b) => b.revenue - a.revenue);
+
+  console.log(sortedRevenues);
+  console.log(sortedRevenues.map(product => product.name).slice(0, 3));
+
+  //return sortedRevenues.map(product => product.name).slice(0, 3);
+  
+  /* const revenue = [];
   const topGrossing = [];
 
   amounts.forEach((amount, i) => {
@@ -24,8 +33,21 @@ function top3(products, amounts, prices) {
   }
 
   console.log(topGrossing.slice(0, 3));
-  //return topGrossing;
+  //return topGrossing; */
 
 }
 
-top3(["Cell Phones", "Vacuum Cleaner", "Computer", "Autos", "Gold", "Fishing Rods", "Lego", "Speakers"], [5, 25, 2, 7, 10, 3, 2, 24], [51, 225, 22, 47, 510, 83, 82, 124]);
+top3(
+  [
+    "Cell Phones",
+    "Vacuum Cleaner",
+    "Computer",
+    "Autos",
+    "Gold",
+    "Fishing Rods",
+    "Lego",
+    " Speakers"
+  ],
+  [0, 12, 24, 17, 19, 23, 120, 8],
+  [9, 24, 29, 31, 51, 8, 120, 14]
+);
