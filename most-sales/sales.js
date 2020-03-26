23464c4e1be0cb82d6d2985a42d572ed9ca62550
Mariella Miranda <mariella@able.co>
2020-03-26 09:11:30 -0500

    Clean up file

--- most-sales/sales.js
+++ most-sales/sales.js
@@ -1,7 +1,3 @@
-/* products: ["Computer", "Cell Phones", "Vacuum Cleaner"]
-amounts: [3, 24, 8]
-prices: [199, 299, 399] */
-
 function top3(products, amounts, prices) {
 
   const totalRevenues = products.map((product, i) => ({id: i, name: product, revenue: amounts[i] * prices[i]}));
@@ -11,30 +7,6 @@ function top3(products, amounts, prices) {
   console.log(sortedRevenues.map(product => product.name).slice(0, 3));
 
   //return sortedRevenues.map(product => product.name).slice(0, 3);
-  
-  /* const revenue = [];
-  const topGrossing = [];
-
-  amounts.forEach((amount, i) => {
-    const individualRevenue = amount * prices[i];
-    revenue.push(individualRevenue);
-  })
-
-  for (i=0; i<prices.length; i++){
-    products.find(function (product, index) {
-      let indexToRemove = revenue.indexOf(Math.max(...revenue));
-
-      if (index == indexToRemove) {
-        topGrossing.push(product);
-        products.splice(indexToRemove, 1);
-        revenue.splice(indexToRemove, 1);
-      }
-    });
-  }
-
-  console.log(topGrossing.slice(0, 3));
-  //return topGrossing; */
-
 }
 
 top3(
