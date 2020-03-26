c49d5dba8f406f99a8613c35f7f6e9ddb42a6ab8
Mariella Miranda <mariella@able.co>
2020-03-26 10:21:34 -0500

    reworking the function with map

--- spinning-words/spin.js
+++ spinning-words/spin.js
@@ -6,8 +6,20 @@
 */
 function spinWords(s) {
   const words = s.split(' ');
-  const wordsSpinned = [];
 
+  // WITH map
+  const wordsSpinned = words.map(function(word){
+    if (word.length > 4) {
+      return word
+        .split("")
+        .reverse()
+        .join("");
+    }
+    return word;
+  })
+
+
+  /* WITH forEach
   words.forEach(word =>
     word.length > 4
       ? wordsSpinned.push(
@@ -17,11 +29,11 @@ function spinWords(s) {
             .join("")
         )
       : wordsSpinned.push(word)
-  );
+  ); */
 
   //console.log(words);
   //console.log(wordsSpinned.join(' '));
 
-  return wordsSpinned.join(" ");
+  return wordsSpinned.join(' ');
 }
 spinWords("Just kidding there is still one more"); //"Just gniddik ereht is llits one more"
