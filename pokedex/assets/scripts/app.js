777af27f9a6f862ecadeea73e144577fa476dcc0
Mariella Miranda <mariella@able.co>
2020-04-02 14:00:50 -0500

    Optimizing data fetch

--- pokedex/assets/scripts/app.js
+++ pokedex/assets/scripts/app.js
@@ -7,27 +7,26 @@ function append(parent, el) {
 }
 
 const list = document.getElementById('pokedex');
-const url = 'https://pokeapi.co/api/v2/pokemon';
+const url = "https://pokeapi.co/api/v2/pokemon/?limit=150";
 
-for (i = 1; i <= 150; i++){
-  fetch(url + '/' + i)
-    .then(resp => resp.json())
-    .then(function(data) {
-      let pokemon = data;
+fetch(url)
+  .then(resp => resp.json())
+  .then(function(data) {
+    var pokemons = data.results;
+    pokemons.forEach((e, i) => {
       let li = createNode('li'),
           img = createNode('img'),
-          span = createNode('span'),
-          a = createNode('a');
-      
-      a.href = url + '/' + i;
-      img.src = pokemon.sprites.front_default;
-      span.innerHTML = pokemon.name;
+          h2 = createNode('h2'),
+          a = createNode('a'),
+          span = createNode('span');
+      a.href = e.url;
+      span.innerHTML = i+1;
+      img.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+(i+1)+'.png';
+      h2.innerHTML = e.name;
       append(li, a);
-      append(a, img);
       append(a, span);
+      append(a, img);
+      append(a, h2);
       append(list, li);
-      })
-    .catch(function(error) {
-      console.log(error);
     });
-}
+  })
