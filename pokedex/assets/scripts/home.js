332aca68c0f104242060a25e3b0c8f3f3e7b69b3
Mariella Miranda <mariella@able.co>
2020-04-03 14:31:57 -0500

    Styling and adding search input

--- pokedex/assets/scripts/home.js
+++ pokedex/assets/scripts/home.js
@@ -12,14 +12,14 @@ const url = "https://pokeapi.co/api/v2/pokemon/?limit=150";
 fetch(url)
   .then(resp => resp.json())
   .then(function(data) {
-    var pokemons = data.results;
+    let pokemons = data.results;
     pokemons.forEach((e, i) => {
       let li = createNode('li'),
           a = createNode('a'),
           div = createNode('div'),
           h2 = createNode('h2'),
           img = createNode('img');
-      a.href = e.url;
+      a.href = 'detail.html';
       div.innerHTML = '<span>'+ (i+1) + '</span>';
       h2.innerHTML = e.name;
       img.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+(i+1)+'.png';
