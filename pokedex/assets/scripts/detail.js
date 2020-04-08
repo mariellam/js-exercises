b30c85aec10c9ee383f5bf9d9fe8859c77ebd2c9
Maku Miranda <mariella.miranda@gmail.com>
2020-04-07 22:56:53 -0500

    Added styles to detail

--- pokedex/assets/scripts/detail.js
+++ pokedex/assets/scripts/detail.js
@@ -22,25 +22,28 @@ const showPokemon = async() => {
   await fetchPokemon();
 
   const div1 = createNode('div'),
-        div2 = createNode('div'),
+        span = createNode('span'),
         h2 = createNode('h2'),
         img = createNode('img'),
+        div2 = createNode('div'),
         p = createNode('p'),
         pokemonId = pokemon.id,
         pokemonName = pokemon.name,
         pokemonTypes = pokemon.types.map(function(e) {
           return ' ' + e.type.name;
         });
-  div1.innerHTML = '<span>'+ pokemonId + '</span>';
-  div2.setAttribute('id','details');
+  div1.setAttribute('id', 'card');
+  span.innerHTML = pokemonId;
   h2.innerHTML = pokemonName;
-  p.innerHTML = pokemonTypes.toString();
   img.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+ pokemonId +'.png';
-  append(div1, div2);
+  div2.setAttribute('id', 'details');
+  p.innerHTML = '<b>Type:</b> ' + pokemonTypes.toString();
+  append(div1, span);
+  append(span, h2);
   append(div1, img);
-  append(div2, h2);
   append(div2, p);
   append(content, div1);
+  append(content, div2);
 } 
 
 const fetchPokemonFlavor = async() => {
@@ -60,7 +63,7 @@ const showFlavor = async() => {
   const p = createNode('p'),
         pokemonFlavor = pokemonSpecies.flavor_text_entries.find(e => e.language.name == "en").flavor_text;
   p.innerHTML = pokemonFlavor;
-  div2.appendChild(p);
+  append(div2, p);
  
 }
 
