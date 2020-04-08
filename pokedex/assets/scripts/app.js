43ce31ecdd7fdf9d74168fb373856bc959598e0e
Maku Miranda <mariella.miranda@gmail.com>
2020-04-07 22:19:50 -0500

    Search filter working and no errors on detail

--- pokedex/assets/scripts/app.js
+++ pokedex/assets/scripts/app.js
@@ -6,34 +6,52 @@ function append(parent, el) {
   return parent.appendChild(el);
 }
 
-const list = document.getElementById('pokedex');
+const pokeList = document.getElementById('pokedex');
+const searchBox = document.getElementById('searchbox');
 const url = "https://pokeapi.co/api/v2/pokemon/?limit=150";
 let pokemons;
+let searchTerm = '';
 
-fetch(url)
-  .then(resp => resp.json())
+const fetchPokemons = async() => {
+  pokemonsFetched = await fetch(url)
+  .then(res => res.json())
   .then(function(data) {
     pokemons = data.results;
-    pokemons.forEach((e, i) => {
-      const li = createNode('li'),
-            a = createNode('a'),
-            div = createNode('div'),
-            h2 = createNode('h2'),
-            img = createNode('img'),
-            pokemonId = i + 1;
-            pokemonName = e.name;
-      a.href = 'detail.html' + '?' + 'name=' + pokemonName;
-      div.innerHTML = '<span>'+ pokemonId + '</span>';
-      h2.innerHTML = e.name;
-      img.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+ pokemonId +'.png';
-      append(li, a);
-      append(a, div);
-      append(a, img);
-      append(div, h2);
-      append(list, li);
-    });
-  })
-.then(function(search){
-  const searchBox = document.getElementById('searchbox');
-  console.log(searchBox);
+  });
+}
+
+const showPokemons = async() => {
+  pokeList.innerHTML = '';
+
+  await fetchPokemons();
+
+  pokemons
+  .filter(pokemon =>
+    pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+  ) 
+  .forEach((pokemon, i) => {
+    const li = createNode('li'),
+          a = createNode('a'),
+          div = createNode('div'),
+          h2 = createNode('h2'),
+          img = createNode('img'),
+          pokemonId = pokemon.url.slice(34, -1);
+          pokemonName = pokemon.name;
+    a.href = 'detail.html' + '?' + 'name=' + pokemonName;
+    div.innerHTML = '<span>'+ pokemonId + '</span>';
+    h2.innerHTML = pokemonName;
+    img.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+ pokemonId +'.png';
+    append(li, a);
+    append(a, div);
+    append(a, img);
+    append(div, h2);
+    append(pokeList, li);
+  });
+}
+
+showPokemons();
+
+searchBox.addEventListener('input', e => {
+  searchTerm = e.target.value;
+  showPokemons();
 });
--- pokedex/assets/scripts/detail.js
+++ pokedex/assets/scripts/detail.js
@@ -9,43 +9,59 @@ function append(parent, el) {
 const content = document.getElementById('content');
 const name = window.location.search.slice(6);
 const urlBasic = 'https://pokeapi.co/api/v2/pokemon/' + name;
-
-fetch(urlBasic)
-  .then(resp => resp.json())
-  .then(function(data) {
-    const pokemon = data;
-    const div1 = createNode('div'),
-          div2 = createNode('div'),
-          h2 = createNode('h2'),
-          img = createNode('img'),
-          p = createNode('p'),
-          pokemonId = pokemon.id,
-          pokemonName = pokemon.name,
-          pokemonTypes = pokemon.types.map(function(e) {
-            return ' ' + e.type.name;
-          });
-      div1.innerHTML = '<span>'+ pokemonId + '</span>';
-      div2.setAttribute('id','details');
-      h2.innerHTML = pokemon.name;
-      p.innerHTML = pokemonTypes.toString();
-      img.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+ pokemonId +'.png';
-      append(div1, div2);
-      append(div1, img);
-      append(div2, h2);
-      append(div2, p);
-      append(content, div1);
-      return pokemonName;
-  });
-
 const urlFlavor = 'https://pokeapi.co/api/v2/pokemon-species/' + name;
+let pokemon;
 
-fetch(urlFlavor)
-  .then(resp => resp.json())
+const fetchPokemon = async() => {
+  pokemon = await fetch(urlBasic)
+  .then(res => res.json());
+}
+
+const showPokemon = async() => {
+
+  await fetchPokemon();
+
+  const div1 = createNode('div'),
+        div2 = createNode('div'),
+        h2 = createNode('h2'),
+        img = createNode('img'),
+        p = createNode('p'),
+        pokemonId = pokemon.id,
+        pokemonName = pokemon.name,
+        pokemonTypes = pokemon.types.map(function(e) {
+          return ' ' + e.type.name;
+        });
+  div1.innerHTML = '<span>'+ pokemonId + '</span>';
+  div2.setAttribute('id','details');
+  h2.innerHTML = pokemonName;
+  p.innerHTML = pokemonTypes.toString();
+  img.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+ pokemonId +'.png';
+  append(div1, div2);
+  append(div1, img);
+  append(div2, h2);
+  append(div2, p);
+  append(content, div1);
+} 
+
+const fetchPokemonFlavor = async() => {
+  pokemonsFetched = await fetch(urlFlavor)
+  .then(res => res.json())
   .then(function(data) {
-    const pokemonSpecies = data;
-    const div2 = document.getElementById('details');
-    const p = createNode('p'),
-          pokemonFlavor = pokemonSpecies.flavor_text_entries.find(e => e.language.name == "en").flavor_text;
-      p.innerHTML = pokemonFlavor;
-      div2.appendChild(p);
+    pokemonSpecies = data;
   });
+} 
+
+const showFlavor = async() => {
+
+  await showPokemon();
+  await fetchPokemonFlavor();
+
+  const div2 = document.getElementById('details');
+  const p = createNode('p'),
+        pokemonFlavor = pokemonSpecies.flavor_text_entries.find(e => e.language.name == "en").flavor_text;
+  p.innerHTML = pokemonFlavor;
+  div2.appendChild(p);
+ 
+}
+
+showFlavor();
